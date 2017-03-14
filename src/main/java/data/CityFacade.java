0a39ea57002c4f645971d41cc72b95735b7ede8e
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-03-14 12:43:05 +0100

    added Docs to CityFacade

--- src/main/java/data/CityFacade.java
+++ src/main/java/data/CityFacade.java
@@ -18,21 +18,41 @@ public class CityFacade implements ICityFacade {
     private EntityManagerFactory emf = Persistence
             .createEntityManagerFactory("PU");
 
+    /**
+     * Add an Entity Manager Factory which will be used for
+     * when generating Entity Managers for the other methods
+     * @param emf
+     */
     @Override
     public void addEntityManagerFactory(EntityManagerFactory emf) {
         this.emf = emf;
     }
 
+    /**
+     * Get a single city by it's Identity id
+     * @param id Identity id
+     * @return single city by id
+     */
     @Override
     public City getCity(int id) {
         return getCity(((long) id));
     }
 
+    /**
+     * Get a single city by it's Identity id
+     * @param id Identity id
+     * @return single city by id
+     */
     @Override
     public City getCity(long id) {
         return find(id);
     }
 
+    /**
+     * Get a single city by it's zip code
+     * @param zipCode zip code
+     * @return single city by zip code
+     */
     @Override
     public City getCity(String zipCode) {
         EntityManager em = emf.createEntityManager();
@@ -43,6 +63,10 @@ public class CityFacade implements ICityFacade {
         return query.getSingleResult();
     }
 
+    /**
+     * Get all cities
+     * @return all cities
+     */
     @Override
     public List<City> getCities() {
         EntityManager em = emf.createEntityManager();
@@ -51,8 +75,14 @@ public class CityFacade implements ICityFacade {
         return query.getResultList();
     }
 
+    /**
+     * Find City by Id
+     * @param id Identity Id
+     * @return City with given Id
+     */
     City find(Long id) {
         EntityManager em = emf.createEntityManager();
         return em.find(City.class, id);
     }
+
 }
