0256fa95e2b2de227e8eb2db51fda7d2e9edcc03
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-03-14 12:29:50 +0100

    updated PersonResouce

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -8,6 +8,7 @@ package rest;
 import com.google.gson.Gson;
 import data.IPersonFacade;
 import data.PersonFacade;
+import entity.Person;
 
 import javax.persistence.Persistence;
 import javax.ws.rs.*;
@@ -41,6 +42,64 @@ public class PersonResource {
     @Produces(MediaType.APPLICATION_JSON)
     public String getPersons() {
         return gson.toJson(facade.getPersons());
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPersonById(@PathParam("id") int id) {
+        return gson.toJson(facade.getPerson(id));
+    }
+
+    /**
+     * TODO: make it not return hobbies
+     *
+     * @return all persons
+     */
+    @GET
+    @Path("contactinfo")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPersonsContactInfo() {
+        return gson.toJson(facade.getPersons());
+    }
+
+    /**
+     * TODO: make it not return hobbies
+     *
+     * @param id
+     * @return single person
+     */
+    @GET
+    @Path("contactinfo/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPersonContactInfoById(@PathParam("id") int id) {
+        return gson.toJson(facade.getPerson(id));
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String addPerson(String json) {
+        Person person = gson.fromJson(json, Person.class);
+        facade.addPerson(person);
+        return gson.toJson(person);
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String editPerson(String json) {
+        Person person = gson.fromJson(json, Person.class);
+        facade.editPerson(person);
+        return gson.toJson(person);
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String deletePerson(@PathParam("id") int id) {
+        facade.deletePerson(id);
+        return "{\"message\":\"Deleted person with id: \"" + id + "}";
     }
 
 }
